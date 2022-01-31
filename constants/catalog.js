14c175198f6fa2322ee68d831637c5a8b70778e5
elenagaranina <garanina1700@yandex.ru>
2022-01-31 20:05:36 +0300

    structure

---
+++ constants/catalog.js
@@ -0,0 +1,99 @@
+const CATALOG =[
+    {
+        id:'el1',
+        name:'Увлажнитель воздух STARWIND SHC1322, 3л, белый',
+        img: 'https://github.com/elenagaranina/imgs/blob/main/Товар1.png?raw=true',
+        price: 1650,
+    },
+    {
+        id:'el2',
+        name:'Триммер PHILIPS HC3521/15 серебристый/черный',
+        img: 'https://github.com/elenagaranina/imgs/blob/main/Товар2.png?raw=true',
+        price: 2290,
+    },
+    {
+        id:'el3',
+        name:'',
+        img: '',
+        price: 0,
+    },
+    {
+        id:'el4',
+        name:'',
+        img: '',
+        price: 0,
+    },
+    {
+        id:'el5',
+        name:'',
+        img: '',
+        price: 0,
+    },
+    {
+        id:'el6',
+        name:'',
+        img: '',
+        price: 0,
+    },
+    {
+        id:'el7',
+        name:'',
+        img: '',
+        price: 0,
+    },
+    {
+        id:'el8',
+        name:'',
+        img: '',
+        price: 0,
+    },
+    {
+        id:'el9',
+        name:'',
+        img: '',
+        price: 0,
+    },
+    {
+        id:'el10',
+        name:'',
+        img: '',
+        price: 0,
+    },
+    {
+        id:'el11',
+        name:'',
+        img: '',
+        price: 0,
+    },
+    {
+        id:'el12',
+        name:'',
+        img: '',
+        price: 0,
+    },
+    {
+        id:'el13',
+        name:'',
+        img: '',
+        price: 0,
+    },
+    {
+        id:'el14',
+        name:'',
+        img: '',
+        price: 0,
+    },
+    {
+        id:'el15',
+        name:'',
+        img: '',
+        price: 0,
+    },
+    {
+        id:'el16',
+        name:'',
+        img: '',
+        price: 0,
+    },
+
+];
